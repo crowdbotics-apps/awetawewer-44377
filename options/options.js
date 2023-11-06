a04af5d3b3f72e9d97c85161904a38eae38c9f7e
Crowdbotics <team@crowdbotics.com>
2023-11-06 22:03:51 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "awetawewer-44377",
+  url: "https://awetawewer-44377.botics.co",
+  api: "https://awetawewer-44377.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
